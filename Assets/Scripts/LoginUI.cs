e6cd0bded4f8c0ba910aff88e09b8f3326637433
Unknown <martyushevsky@gmail.com>
2019-02-09 23:32:31 +0300

    Lesson 1 (9)

---
+++ Assets/Scripts/LoginUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class LoginUI : MonoBehaviour
+{
+    [SerializeField] GameObject loginPanel;
+
+    private void Start()
+    {
+        if ((NetworkManager.singleton as MyNetworkManager).serverMode)
+            loginPanel.SetActive(false);
+    }
+
+    public void Login()
+    {
+        NetworkManager.singleton.StartClient();
+    }
+}
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,130 +3,130 @@ using UnityEngine.Networking;
 
 namespace Geekbrains
 {
-	public class Unit : Interactable
-	{
-		[SerializeField] protected UnitMotor Motor;
-		[SerializeField] protected UnitStats _stats;
+    public class Unit : Interactable
+    {
+        [SerializeField] protected UnitMotor Motor;
+        [SerializeField] protected UnitStats _stats;
 
-		public UnitStats Stats => _stats;
+        public UnitStats Stats => _stats;
 
-		protected Interactable Focus;
+        protected Interactable Focus;
 
-		protected bool IsDead;
+        protected bool IsDead;
 
-		public delegate void UnitDenegate();
-		[SyncEvent] public event UnitDenegate EventOnDamage;
-		[SyncEvent] public event UnitDenegate EventOnDie;
-		[SyncEvent] public event UnitDenegate EventOnRevive;
+        public delegate void UnitDelegate();
+        public event UnitDelegate EventOnDamage;
+        public event UnitDelegate EventOnDie;
+        public event UnitDelegate EventOnRevive;
 
-		public override void OnStartServer()
-		{
-			Motor.SetMoveSpeed(_stats.MoveSpeed.GetValue());
-			_stats.MoveSpeed.OnStatChanged += Motor.SetMoveSpeed;
-		}
+        public override void OnStartServer()
+        {
+            Motor.SetMoveSpeed(_stats.MoveSpeed.GetValue());
+            _stats.MoveSpeed.OnStatChanged += Motor.SetMoveSpeed;
+        }
 
-		private void Update()
-		{
-			OnUpdate();
-		}
+        private void Update()
+        {
+            OnUpdate();
+        }
 
-		protected virtual void OnAliveUpdate()
-		{
-		}
+        protected virtual void OnAliveUpdate()
+        {
+        }
 
-		protected virtual void OnDeadUpdate()
-		{
-		}
+        protected virtual void OnDeadUpdate()
+        {
+        }
 
-		protected void OnUpdate()
-		{
-			if (!isServer) return;
-			if (!IsDead)
-			{
-				if (_stats.CurHealth == 0) Die();
-				else OnAliveUpdate();
-			}
-			else
-			{
-				OnDeadUpdate();
-			}
-		}
+        protected void OnUpdate()
+        {
+            if (!isServer) return;
+            if (!IsDead)
+            {
+                if (_stats.CurHealth == 0) Die();
+                else OnAliveUpdate();
+            }
+            else
+            {
+                OnDeadUpdate();
+            }
+        }
 
-		/// <summary>
-		/// Установка нового объекта в фокус
-		/// </summary>
-		/// <param name="newFocus"></param>
-		protected virtual void SetFocus(Interactable newFocus)
-		{
-			if (newFocus == Focus) return;
-			Focus = newFocus;
-			Motor.FollowTarget(newFocus);
-		}
+        /// <summary>
+        /// Установка нового объекта в фокус
+        /// </summary>
+        /// <param name="newFocus"></param>
+        protected virtual void SetFocus(Interactable newFocus)
+        {
+            if (newFocus == Focus) return;
+            Focus = newFocus;
+            Motor.FollowTarget(newFocus);
+        }
 
-		/// <summary>
-		/// Удаление фокуса
-		/// </summary>
-		protected virtual void RemoveFocus()
-		{
-			Focus = null;
-			Motor.StopFollowingTarget();
-		}
+        /// <summary>
+        /// Удаление фокуса
+        /// </summary>
+        protected virtual void RemoveFocus()
+        {
+            Focus = null;
+            Motor.StopFollowingTarget();
+        }
 
-		[ClientCallback]
-		protected virtual void Die()
-		{
-			IsDead = true;
-			GetComponent<Collider>().enabled = false;
-			if (!isServer) return;
-			HasInteract = false; // с объектом нельзя взаимодействовать
-			RemoveFocus();
-			Motor.MoveToPoint(transform.position);
-			EventOnDie?.Invoke();
-			RpcDie();
-		}
+        [ClientCallback]
+        protected virtual void Die()
+        {
+            IsDead = true;
+            GetComponent<Collider>().enabled = false;
+            if (!isServer) return;
+            HasInteract = false; // с объектом нельзя взаимодействовать
+            RemoveFocus();
+            Motor.MoveToPoint(transform.position);
+            EventOnDie?.Invoke();
+            RpcDie();
+        }
 
-		[ClientRpc]
-		private void RpcDie()
-		{
-			if (!isServer) Die();
-		}
+        [ClientRpc]
+        private void RpcDie()
+        {
+            if (!isServer) Die();
+        }
 
-		[ClientCallback]
-		protected virtual void Revive()
-		{
-			IsDead = false;
-			GetComponent<Collider>().enabled = true;
-			if (!isServer) return;
-			HasInteract = true; // с объектом можно взаимодействовать
-			_stats.SetHealthRate(1);
-			EventOnRevive?.Invoke();
-			RpcRevive();
-		}
+        [ClientCallback]
+        protected virtual void Revive()
+        {
+            IsDead = false;
+            GetComponent<Collider>().enabled = true;
+            if (!isServer) return;
+            HasInteract = true; // с объектом можно взаимодействовать
+            _stats.SetHealthRate(1);
+            EventOnRevive?.Invoke();
+            RpcRevive();
+        }
 
-		[ClientRpc]
-		private void RpcRevive()
-		{
-			if (!isServer) Revive();
-		}
+        [ClientRpc]
+        private void RpcRevive()
+        {
+            if (!isServer) Revive();
+        }
 
-		public override bool Interact(GameObject user)
-		{
-			Debug.Log(gameObject.name + " ineracted with " + user.name);
-			Combat combat = user.GetComponent<Combat>();
-			if (combat != null)
-			{
-				if (combat.Attack(_stats))
-				{
-					DamageWithCombat(user);
-				}
-				return true;
-			}
-			return base.Interact(user);
-		}
+        public override bool Interact(GameObject user)
+        {
+            Debug.Log(gameObject.name + " ineracted with " + user.name);
+            Combat combat = user.GetComponent<Combat>();
+            if (combat != null)
+            {
+                if (combat.Attack(_stats))
+                {
+                    DamageWithCombat(user);
+                }
+                return true;
+            }
+            return base.Interact(user);
+        }
 
-		protected virtual void DamageWithCombat(GameObject user)
-		{
-			EventOnDamage?.Invoke();
-		}
-	}
+        protected virtual void DamageWithCombat(GameObject user)
+        {
+            EventOnDamage?.Invoke();
+        }
+    }
 }
